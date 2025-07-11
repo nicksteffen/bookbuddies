81a74c0e40f0babf9fcdd60904346d295e79c361
Nicholas Steffen <nicholassteffen@Nicholass-MacBook-Pro.local>
2025-07-11 12:59:48 -0400

    update icon for home

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Chrome as Home, Users, Library, User } from 'lucide-react-native';
+import { HomeIcon as Home, Users, Library, User } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
